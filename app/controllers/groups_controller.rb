4040843a6d39bd61d0fbe2cdcd72e5356e50f8c0
Katarzyna Wrzesinska <k.m.wrzesinska@gmail.com>
2013-07-27 11:57:14 +0200

    Start new form

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,9 +3,30 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def new
+    @group = Group.new
+  end
   
+  def create
+    @group = Group.new(group_params)
+     if @group.save
+        redirect_to root_path, alert: 'Group was successfully created.'
+     else
+        render action: "new"
+     end
+  end
+  
+  # had some problems here, seems like Rails 4 requires 
+  # defining parameters in separate method and then passing that in CREATE (?) 
+  def group_params
+     params.require(:group).permit(:name, :address, :time, :number_of_members)
+  end
+  
+    
   def show
     
   end
+  
   
 end
