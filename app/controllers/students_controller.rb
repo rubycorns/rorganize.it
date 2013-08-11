2d951c3ea9df9ab89ee111b58726c799eca69dc4
Lisa P <passinglisa@gmail.com>
2013-08-11 19:28:22 +0200

    add student new functionality

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,17 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def create
+    @student = Student.new(student_params)
+     if @student.save
+        redirect_to students_path, alert: 'New student successfully registered!'
+     else
+        render action: "new"
+     end
+  end
+
+  def student_params
+     params.require(:student).permit(:first_name, :last_name, :email, :group_id)
+  end
+
 end
