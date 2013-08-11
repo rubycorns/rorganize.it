2078901ff39e97c803e786e562f46629f7e1c91c
Lisa P <passinglisa@gmail.com>
2013-08-11 18:41:18 +0200

    delete 'student', generate 'students'

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,4 @@
-class StudentController < ApplicationController
+class StudentsController < ApplicationController
 
   def index
     @students = Student.all
--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class StudentControllerTest < ActionController::TestCase
+class StudentsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
