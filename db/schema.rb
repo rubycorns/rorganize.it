8cd9741b41753baa15c4c40e904141e2e3e513ca
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-09-17 20:26:14 +0200

    Added picture to persons because somehow it should be there, but isnt :-(

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130813184733) do
+ActiveRecord::Schema.define(version: 20130917182417) do
 
   create_table "groups", force: true do |t|
     t.string   "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20130813184733) do
     t.integer  "group_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "picture"
   end
 
   add_index "people", ["group_id"], name: "index_people_on_group_id"
