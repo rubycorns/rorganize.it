07396c09d5d0949339a558df87c732ba04a72602
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-09-17 20:19:14 +0200

    reset schema.rb to a non devise state

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130910154704) do
+ActiveRecord::Schema.define(version: 20130813184733) do
 
   create_table "groups", force: true do |t|
     t.string   "name"
@@ -26,25 +26,13 @@ ActiveRecord::Schema.define(version: 20130910154704) do
   create_table "people", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email",                  default: "", null: false
+    t.string   "email"
     t.string   "type"
     t.integer  "group_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "picture"
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
   end
 
-  add_index "people", ["email"], name: "index_people_on_email", unique: true
   add_index "people", ["group_id"], name: "index_people_on_group_id"
-  add_index "people", ["reset_password_token"], name: "index_people_on_reset_password_token", unique: true
 
 end
