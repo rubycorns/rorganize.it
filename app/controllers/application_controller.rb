856d815d321b0af71d02a089806d5f1c9c3905e0
Lisa P <passinglisa@gmail.com>
2013-10-01 21:09:52 +0200

    customize devise
    we need to specify person type on registration with a dropdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,13 @@ class ApplicationController < ActionController::Base
 
   before_filter :authenticate_person!
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :type
+    devise_parameter_sanitizer.for(:sign_up) << :first_name
+  end
+
 end
