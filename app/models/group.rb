6bae65a634f46209da9349ef4d73e6f7189f33be
Lisa P <passinglisa@gmail.com>
2013-10-28 22:07:07 +0100

    add twitter helper to groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,5 +7,25 @@ class Group < ActiveRecord::Base
   def twitter=(handle)
     self[:twitter] = handle.gsub('@', '');
   end
+
+  def has_twitter
+    twitter && twitter != ''
+  end
+
+  def twitter_handle
+    if has_twitter
+      twitter_handle = '@' + twitter
+    else
+      ''
+    end
+  end
+
+  def twitter_profile_url
+    if has_twitter
+      twitter_profile_url = 'https://twitter.com/' + twitter
+    else
+      ''
+    end
+  end
   
 end
