d6955af1cad904ca239de10177011b8db8ecff42
Lisa P <passinglisa@gmail.com>
2014-01-04 19:35:55 +0100

    reverse schema and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-groups = Group.create([
-  { name: 'The Thursday Group', address: 'modomoto office, Blücherstraße 22, 10961 Berlin' },
-  { name: 'The Rubycorns', address: 'Travis Office, Prinzessinnenstr. 20, 10969 Berlin' },
-  { name: 'The Co.Up Group', address: 'co.up Coworking Adalbertstraße 8, 10999 Berlin' },
-  { name: 'The Ruby Monsters', address: 'Travis Office, Prinzessinnenstr. 20, 10969 Berlin' }])
