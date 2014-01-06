8c517a49c9794616bc24b448a76e030005427c6f
Lisa P <passinglisa@gmail.com>
2014-01-06 19:50:24 +0100

    make array of hashes instead of array of arrays

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,36 +7,36 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 group_list = [
-  [
-    'The Thursday Group',
-    'modomoto office, Blücherstraße 22, 10961 Berlin',
-    'Thursday 19:00',
-    'https://groups.google.com/forum/#!forum/rails-girls-berlin-project-group-thursdays',
-    'We build a wishlist app to organize christmas for large families. Users can enter their own wishlist and add wishes. Other users can comment on these wishes, mark them as done etc, but the list owner can’t see that of course.'
-  ],
-  [
-    'The Rubycorns',
-    'Travis Office, Prinzessinnenstr. 20, 10969 Berlin',
-    'Tuesday 19:00',
-    'https://groups.google.com/forum/#!forum/rails-girls-berlin-project-group-tuesdays',
-    'We basically meet and have coding sessions where we show of stuff as needed. Sometimes Tobi gives a presentation from his HU course or something. Basically everything including homeworks is pretty ad-hoc.'
-  ],
-  [
-    'The Co.Up Group',
-    'co.up Coworking Adalbertstraße 8, 10999 Berlin',
-    'Tuesday 19:00',
-    '',
-    'Nothing specific, we do exercises around basic web apps with Sinatra.'
-  ],
-  [
-    'The Ruby Monsters',
-    'Travis Office, Prinzessinnenstr. 20, 10969 Berlin',
-    'Monday evening',
-    'https://groups.google.com/forum/#!members/rubymonstaaa',
-    'The group is now a mixture of old-timers and newcomers, and from the original group some are still working on SpeakerinnenListe, others are doing the Michael Hartl tutorial, and the newcomers have been doing general Ruby exercises. A sub-group is doing the Michael Hartl tutorial'
-  ]
+  {
+    name: 'The Thursday Group',
+    address: 'modomoto office, Blücherstraße 22, 10961 Berlin',
+    time: 'Thursday 19:00',
+    contact: 'https://groups.google.com/forum/#!forum/rails-girls-berlin-project-group-thursdays',
+    activities: 'We build a wishlist app to organize christmas for large families. Users can enter their own wishlist and add wishes. Other users can comment on these wishes, mark them as done etc, but the list owner can’t see that of course.'
+  },
+  {
+    name: 'The Rubycorns',
+    address: 'Travis Office, Prinzessinnenstr. 20, 10969 Berlin',
+    time: 'Tuesday 19:00',
+    contact: 'https://groups.google.com/forum/#!forum/rails-girls-berlin-project-group-tuesdays',
+    activities: 'We basically meet and have coding sessions where we show of stuff as needed. Sometimes Tobi gives a presentation from his HU course or something. Basically everything including homeworks is pretty ad-hoc.'
+  },
+  {
+    name: 'The Co.Up Group',
+    address: 'co.up Coworking Adalbertstraße 8, 10999 Berlin',
+    time: 'Tuesday 19:00',
+    contact: '',
+    activities: 'Nothing specific, we do exercises around basic web apps with Sinatra.'
+  },
+  {
+    name: 'The Ruby Monsters',
+    address: 'Travis Office, Prinzessinnenstr. 20, 10969 Berlin',
+    time: 'Monday evening',
+    contact: 'https://groups.google.com/forum/#!members/rubymonstaaa',
+    activities: 'The group is now a mixture of old-timers and newcomers, and from the original group some are still working on SpeakerinnenListe, others are doing the Michael Hartl tutorial, and the newcomers have been doing general Ruby exercises. A sub-group is doing the Michael Hartl tutorial'
+  }
 ]
 
-group_list.each do |name, address, time, contact, activity|
-  Group.create( name: name, address: address, time: time, contact: contact, activities: activity)
+group_list.each do |group|
+  Group.create!(group)
 end
