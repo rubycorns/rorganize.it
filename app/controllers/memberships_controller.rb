90438a1650e29168605b598b35b3d2acfcb8362a
tam <tam.eastley@gmail.com>
2014-03-28 15:26:31 +0100

    Enable person to leave a group

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -9,4 +9,12 @@ class MembershipsController < ApplicationController
     end
   end
 
+  def update
+    @person = Person.find(current_person.id)
+    if @person.update(group_id: nil)
+      flash[:success] = "You have left your group. No doubt they will miss you. Please consider
+      buying everyone cake on your last day."
+      redirect_to groups_path
+    end
+  end
 end
