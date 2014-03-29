b7ea0dc440372099242659bb3105081b5685a184
Tilmann Singer <tils@tils.net>
2014-03-29 11:04:53 +0100

    Remove require pry call from controller
    I'm quite sure it was added by mistake, and is not necessary.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  require 'pry'
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
