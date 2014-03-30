a54b3536b59736894fb60eb642dd7e7de231b8b0
tam <tam.eastley@gmail.com>
2014-03-30 11:40:23 +0200

    WIP send mail when person joins a group

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,14 @@ RailsGirlsApp::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: '<username>@gmail.com',
+    password: '<password>',
+    authentication: :plain
+  }
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   config.action_mailer.delivery_method = :smtp
