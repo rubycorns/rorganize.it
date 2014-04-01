5d15298293bd62cd46cbfa1bf778cd1d43fb10e2
tam <tam.eastley@gmail.com>
2014-04-01 20:01:04 +0200

    change for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,6 @@ RailsGirlsApp::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
 
 end
