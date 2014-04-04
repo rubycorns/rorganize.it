e100c51069034a3e8639ae2d993e73a1fa1e0746
tam <tam.eastley@gmail.com>
2014-04-04 17:23:51 +0200

    Incorporate new associations into controller

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -3,21 +3,24 @@ class MembershipsController < ApplicationController
 
   def create
     @person = Person.find(current_person.id)
-    @group = Group.find(params[:person][:group_id])
-    if @person.update(group_id: @group.id)
+    @group = Group.find(params[:membership][:group_id])
+
+    @person.join(@group)
       flash[:success] = "Welcome to the group #{@group.name}! Please consider buying everyone cake on your first day."
       PersonMailer.new_member_email(@group, @person).deliver
       redirect_to group_path(@group)
-    end
   end
 
-  def update
+
+  def destroy
     @person = Person.find(current_person.id)
-    if @person.update(group_id: nil)
+    @group = Group.find(params[:membership][:group_id])
+
+    @person.leave(@group)
       flash[:success] = leave_message
       redirect_to groups_path
-    end
   end
+
 
   private
 
