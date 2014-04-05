f563f1a0cf6aac6848852a928914ebba3afcaafd
tam <tam.eastley@gmail.com>
2014-04-05 23:02:07 +0200

    Clean up/remove unnecessary code

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -2,25 +2,26 @@ class MembershipsController < ApplicationController
   before_action :authenticate_person!
 
   def create
-    @person = Person.find(current_person.id)
+    @person = current_person
     @group = Group.find(params[:membership][:group_id])
 
-    @person.join(@group)
+    if @person.join!(@group)
       flash[:success] = "Welcome to the group #{@group.name}! Please consider buying everyone cake on your first day."
       PersonMailer.new_member_email(@group, @person).deliver
       redirect_to group_path(@group)
+    else
+      redirect_to groups_path
+    end
   end
 
-
   def destroy
-    @person = Person.find(current_person.id)
+    @person = current_person
     @group = Group.find(params[:membership][:group_id])
 
-    @person.leave(@group)
+    @person.leave!(@group)
       flash[:success] = leave_message
       redirect_to groups_path
   end
-
 
   private
 
