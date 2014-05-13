2ceda5da8e659abb25f6c90cc7b5e560b0b32c2e
Lisa P <passinglisa@gmail.com>
2014-05-13 21:11:28 +0200

    replace cloudmade as map tile provider. Long live OSM

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,4 +1,4 @@
-Leaflet.tile_layer = "http://{s}.tile.cloudmade.com/6f395d7416f14061badc66d0df0c76ae/997/256/{z}/{x}/{y}.png"
+Leaflet.tile_layer = "http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png"
 # You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
 Leaflet.attribution = "Your attribution statement"
 Leaflet.max_zoom = 18
