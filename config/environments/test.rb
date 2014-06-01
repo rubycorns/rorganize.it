72dc1ba15ccd22691081718854d72204143d1603
tam <tam.eastley@gmail.com>
2014-06-01 18:40:23 +0200

    add tests for membership controller

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ RailsGirlsApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.action_mailer.default_url_options = { :host => 'test.localhost:3000' }
+
 end
