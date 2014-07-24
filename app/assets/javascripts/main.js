776d102a496a74f3cf77bcd73ef274dea098cac6
Lisa P <mail@lislis.de>
2014-07-24 11:43:51 +0200

    fix js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 
 	function removeAlert() {
-		$('.alert').addClass('fade-out').defer(2000).remove();
+		$('.alert').addClass('fade-out');
 	}
 	window.setTimeout(removeAlert, 3000);
 
