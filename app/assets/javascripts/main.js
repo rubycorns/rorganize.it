976b830baa3dd227e5cbebe554bfb11e4fbd203a
Lisa P <mail@lislis.de>
2014-08-12 19:08:05 +0200

    add random gif panel

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,4 +6,15 @@ $(document).ready(function() {
 	}
 	window.setTimeout(removeAlert, 3000);
 
+
+	if($('#js-randomGif').length) {
+		$.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=adventure+time')
+			.done(function(data) {
+				$('#js-randomGif').attr('src', data.data.image_url).on('load', function() {
+					$(this).removeClass('hidden');
+				});
+
+			});
+	}
+	
 });
