a5dd780d3c60fec480aea95f91429454338deaad
Lisa P <mail@lislis.de>
2014-08-13 16:58:10 +0200

    add spinning gif while random gif is loading

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -8,9 +8,11 @@ $(document).ready(function() {
 
 
 	if($('#js-randomGif').length) {
+		$('#js-randomGif').addClass('is-spinning');
 		$.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=adventure+time')
 			.done(function(data) {
-				$('#js-randomGif').attr('src', data.data.image_url).on('load', function() {
+				$('#js-randomGif img').attr('src', data.data.image_url).on('load', function() {
+					$(this).parent().removeClass('is-spinning');
 					$(this).removeClass('hidden');
 				});
 
