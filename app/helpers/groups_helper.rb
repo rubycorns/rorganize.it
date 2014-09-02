f37bbfa5cdbc1b3f00e14f68b3bf4864728c7f26
Lisa P <mail@lislis.de>
2014-09-02 20:05:27 +0200

    refactor groups map to helper

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -15,4 +15,20 @@ module GroupsHelper
       person.member_of?(group)
     end
   end
+
+  def groups_map(group)
+    map(
+      :container_id => "group-map",
+      :center => {
+        :latlng => [group.latitude, group.longitude],
+        :zoom => 16
+      },
+      :markers => [
+        {
+          :latlng => [group.latitude, group.longitude],
+        }
+      ],
+      :attribution => 'Map data &copy; <a href="http://openstreetmap.org" target="_blank">OpenStreetMap contributors</a>,<a href="https://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a>'
+    )
+  end
 end
