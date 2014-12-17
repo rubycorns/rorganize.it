f9456f2fbd09a2f0d8624f24153f77e6c44299c4
Adrian Rangel <adrian.rangel@gmail.com>
2014-12-17 11:59:45 -0600

    remove show path for topics

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ RailsGirlsApp::Application.routes.draw do
   get 'about' => 'pages#about', as: :about
   #resources routing declare all of the common routes for the certain controller (index, new, edit etc...)
   resources :groups do
-    resources :topics
+    resources :topics, except: :show
   end
   resources :people
   resources :memberships, only: [:create, :destroy]
