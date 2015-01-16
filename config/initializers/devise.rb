ae72293d9fae6ebc1db8df0d0264d9de43285bd6
FrauBienenstich <susanne.dewein@gmail.com>
2015-01-16 17:12:51 +0100

    hide sensitive github keys in env variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'rails-girls-berlin-project-group-tuesdays@googlegroups.com'
-
+  config.omniauth :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET']
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
