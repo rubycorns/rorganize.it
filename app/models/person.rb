0dd9319fea01fdd970ef6117a1351d313f0c0130
FrauBienenstich <susanne.dewein@gmail.com>
2015-01-16 17:12:01 +0100

    add s'moar omniauth methodz

--- app/models/person.rb
+++ app/models/person.rb
@@ -29,7 +29,7 @@ class Person < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:github]
 
   has_many :groups, through: :memberships
   has_many :memberships, dependent: :destroy
@@ -72,5 +72,23 @@ class Person < ActiveRecord::Base
   def <=>(other)
     name <=> other.name
   end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20] 
+      user.first_name = auth.info.name.split(' ').first 
+      user.last_name = auth.info.name.split(' ').last
+      user.picture = auth.info.picture
+    end
+  end
+
+  def self.new_with_session(params, session)
+      super.tap do |user|
+        if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
+          user.email = data["email"] if user.email.blank?
+        end
+      end
+    end
 
 end
