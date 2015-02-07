f05f05d7317ce76ede9c41d6ccc5cc32881e56c8
Lisa Passing <mail@lislis.de>
2015-02-07 15:52:36 +0100

    give admins the ability to remove people from groups, closes #300

---
+++ spec/features/admin_remove_person_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+feature 'Remove a person from a group' do
+
+  let(:person) { create(:person) }
+  let(:group) { create(:group) }
+
+  before do
+    visit new_person_session_path
+    sign_in person
+  end
+
+  context 'the non-admin' do
+
+    it 'cannot see the remove form' do
+      person.join!(group, 'StudentMembership')
+      go_to_group_page
+      expect(page).to_not have_selector('.remove-from-group')
+    end
+
+  end
+
+  context 'the admin' do
+
+    before do
+      person.add_role 'admin'
+      person.join!(group, 'StudentMembership')
+      go_to_group_page
+    end
+
+    it 'can see the remove form' do
+      expect(page).to have_selector('.remove-from-group')
+    end
+
+    it 'can remove the person' do
+      within '.group-info' do
+        find(".remove-from-group").click
+      end
+
+      expect(current_path).to eq('/groups')
+      go_to_group_page
+      expect(page).to_not have_selector('.remove-from-group')
+    end
+
+  end
+
+  def go_to_group_page
+    visit '/groups/'
+    click_link 'Test Group'
+  end
+end
