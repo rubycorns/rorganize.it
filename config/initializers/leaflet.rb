e504e1ba1e6f955d17b75ff5d571823eccd03483
Lisa Passing <mail@lislis.de>
2015-03-10 20:14:57 +0100

    switch to map tiles that are hopefully not broken, close #315

--- config/initializers/leaflet.rb
+++ config/initializers/leaflet.rb
@@ -1,4 +1,4 @@
-Leaflet.tile_layer = "http://{s}.www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png"
+Leaflet.tile_layer = "http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png"
 # You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
 Leaflet.attribution = "Your attribution statement"
 Leaflet.max_zoom = 18
