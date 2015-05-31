b2c12b5841781fd56db113c5db94227c5b0697e4
Tilmann Singer <tils@tils.net>
2015-05-31 21:40:17 +0200

    Add example for Post scope spec that actually uses the database
    Please choose if you want to keep this or the one which expects a
    specific generated SQL.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -9,6 +9,41 @@ describe Post do
     end
   end
 
+  describe '.published' do
+    subject { described_class.published }
+
+    it 'includes only published posts' do
+      published = create(:post, draft: false)
+      create(:post, draft: true)
+
+      expect(subject).to contain_exactly(published)
+    end
+
+    it 'sorts by published_on, latest first' do
+      create(:post, draft: false, published_on: '2015-04-01')
+      create(:post, draft: false, published_on: '2015-03-01')
+      create(:post, draft: false, published_on: '2015-05-01')
+
+      expect(subject.map(&:published_on))
+        .to eq([
+          Date.parse('2015-05-01'),
+          Date.parse('2015-04-01'),
+          Date.parse('2015-03-01'),
+        ])
+    end
+  end
+
+  describe '.draft' do
+    subject { described_class.draft }
+
+    it 'includes only draft posts' do
+      create(:post, draft: false)
+      draft = create(:post, draft: true)
+
+      expect(subject).to contain_exactly(draft)
+    end
+  end
+
   it { is_expected.to belong_to(:person) }
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:description) }
