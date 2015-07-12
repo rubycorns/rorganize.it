99a277ceb59971bec490635357e852a584dec0a4
FrauBienenstich <susanne.dewein@gmail.com>
2015-07-12 19:32:52 +0200

    add omniauth callbacks controller

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,15 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def github
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @person = Person.from_omniauth(request.env["omniauth.auth"])
+
+    if @person.persisted?
+      sign_in_and_redirect @person, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
+    else
+      session["devise.github_data"] = request.env["omniauth.auth"]
+      redirect_to new_person_registration_url
+    end
+  end
+end
