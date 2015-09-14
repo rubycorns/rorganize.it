dbca947eb90203a62e7d29ca1c8e6830f2e7d203
berlintam <tam.eastley@makerist.de>
2015-09-14 11:40:52 +0200

    test merge_with_github method

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -146,4 +146,16 @@ describe Person do
     end
   end
 
+  describe '#merge_with_github!' do
+    let(:github_person) { build :person, provider: 'github', uid: '123456' }
+    let(:person) { build :second_person }
+
+    before { person.merge_with_github!(github_person)}
+
+    it 'merges the rorganize person with a github person' do
+      expect(person.reload.provider).to eql 'github'
+      expect(person.reload.uid).to eql '123456'
+    end
+  end
+
 end
