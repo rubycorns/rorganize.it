a0622f47b2cfb5c370167cba0b08225127db4f24
berlintam <tam.eastley@makerist.de>
2016-03-22 21:11:43 +0100

    edit a group as an admin

--- spec/features/group_edit_spec.rb
+++ spec/features/group_edit_spec.rb
@@ -1,52 +1,69 @@
 require 'spec_helper'
 
 feature 'edit a group', :vcr => {:cassette_name => "create_group" } do
-
   before { visit new_person_session_path }
   before { sign_in person }
 
-  let(:person) { create(:person) }
-  let(:group) { create(:group) }
+  context 'as a member of a group' do
+    let(:person) { create(:person) }
+    let(:group) { create(:group) }
 
-  before { person.join!(group, 'StudentMembership') }
+    before { person.join!(group, 'StudentMembership') }
 
-  scenario 'edit the name of an existing group' do
-    visit_group_edit_page
-    expect(page).to_not have_content('Join group as coach')
-    fill_in 'Project group name', with: 'Testgroup'
-    click_button 'Update Group'
+    scenario 'edit the name of an existing group' do
+      visit_group_edit_page_as_a_member
+      expect(page).to_not have_content('Join group as coach')
+      fill_in 'Project group name', with: 'Testgroup'
+      click_button 'Update Group'
 
-    within('.page-header') do
-      expect(page).to have_content('Testgroup')
+      within('.page-header') do
+        expect(page).to have_content('Testgroup')
+      end
+    end
+
+    scenario 'edit the city and country of an existing group' do
+      visit_group_edit_page_as_a_member
+      fill_in 'City', with: 'Not Berlin'
+      select 'France'
+      click_button 'Update Group'
+
+      expect(page).to have_content('Not Berlin, France')
+    end
+
+    scenario 'make current group inactive' do
+      visit_group_edit_page_as_a_member
+      check 'Is your group inactive?'
+      click_button 'Update Group'
+      expect(page).to have_content('GROUP CURRENTLY INACTIVE')
+      visit_group_edit_page_as_a_member
+      check 'Is your group full?'
+      click_button 'Update Group'
+      expect(page).to have_content('GROUP CURRENTLY INACTIVE')
+    end
+
+    def visit_group_edit_page_as_a_member
+      visit root_path
+      click_link 'Groups'
+      expect(page).to have_content('edit')
+      click_link 'edit'
+      expect(page).to have_content('Project group name')
     end
   end
 
-  scenario 'edit the city and country of an existing group' do
-    visit_group_edit_page
-    fill_in 'City', with: 'Not Berlin'
-    select 'France'
-    click_button 'Update Group'
+  context 'as an admin' do
+    let(:person) { create(:admin) }
+    let!(:group) { create(:group, name: 'AWESOME GROUP') }
 
-    expect(page).to have_content('Not Berlin, France')
+    scenario 'edit the group as an admin' do
+      visit_group_edit_page_as_admin
+    end
+
+    def visit_group_edit_page_as_admin
+      visit groups_path
+      click_link 'AWESOME GROUP'
+      expect(page).to have_content('EDIT AS ADMIN')
+      click_link 'EDIT AS ADMIN'
+      expect(page).to have_content('Project group name')
+    end
   end
-
-  scenario 'make current group inactive' do
-    visit_group_edit_page
-    check 'Is your group inactive?'
-    click_button 'Update Group'
-    expect(page).to have_content('GROUP CURRENTLY INACTIVE')
-    visit_group_edit_page
-    check 'Is your group full?'
-    click_button 'Update Group'
-    expect(page).to have_content('GROUP CURRENTLY INACTIVE')
-  end
-
-  def visit_group_edit_page
-    visit root_path
-    click_link 'Groups'
-    expect(page).to have_content('edit')
-    click_link 'edit'
-    expect(page).to have_content('Project group name')
-  end
-
 end
