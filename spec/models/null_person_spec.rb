4fc36d242f83a910a0b76b7124e2929c05909bcf
berlintam <tam.eastley@makerist.de>
2016-04-26 20:18:44 +0200

    some refactoring

--- spec/models/null_person_spec.rb
+++ spec/models/null_person_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe NullPerson do
-  let(:null_person) { NullPerson.new }
+  let(:null_person) { described_class.new }
 
   specify 'is not an admin' do
-    expect(null_person.admin?).to be false
+    expect(null_person.admin?).to be_falsey
   end
 end
