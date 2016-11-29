9fe9b4fecfe2a2bb38c2cfb406ea14aeef2c762a
Katrin <katrin.kampfrath@gmail.com>
2016-11-29 20:08:21 +0100

    Style alerts, remove duplicate buttons

--- app/assets/javascripts/redesign-main.js
+++ app/assets/javascripts/redesign-main.js
@@ -2,20 +2,25 @@
 // lead to 2x assigned param
 // => outcommented for now
 
-// $( document ).ready(function() {
-//   // Tabs
-//   // 1. Hide all tabs except first one when page loads
-//   $(".tab").addClass('hidden');
-//   $(".tab:first").show();
-//
-//   // 2. When tab link is clicked
-//   $('.group-tab a').click(function(e){
-//     e.preventDefault;
-//     // 2.1 Change tab link status to "active"
-//     $('.group-tab a').removeClass('active');
-//     $(this).addClass('active');
-//     // 2.2 Show tab corresponding to the link
-//     $('.tab').hide();
-//     $($(this).attr('href')).show();
-//   });
-// });
+$( document ).ready(function() {
+  setTimeout(function () {
+    $(".alert").addClass("fade-out")
+  }, 8000);
+
+
+  // Tabs
+  // 1. Hide all tabs except first one when page loads
+  // $(".tab").addClass('hidden');
+  // $(".tab:first").show();
+  //
+  // // 2. When tab link is clicked
+  // $('.group-tab a').click(function(e){
+  //   e.preventDefault;
+  //   // 2.1 Change tab link status to "active"
+  //   $('.group-tab a').removeClass('active');
+  //   $(this).addClass('active');
+  //   // 2.2 Show tab corresponding to the link
+  //   $('.tab').hide();
+  //   $($(this).attr('href')).show();
+  // });
+});
