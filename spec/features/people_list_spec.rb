d8dd8f6ad2cc63107dac9e06bc99f270fce6fb01
Sara Regan <regan.sara@gmail.com>
2017-02-21 21:13:38 +0100

    [496] replace tabs with spaces in indentation :P

--- spec/features/people_list_spec.rb
+++ spec/features/people_list_spec.rb
@@ -1,16 +1,15 @@
 require 'spec_helper'
 
 feature 'People List' do
-	
-	let(:person) { create(:person) }
-	let!(:person2) { create(:second_person) }
+ 
+  let(:person) { create(:person) }
+  let!(:person2) { create(:second_person) }
 
-
-	scenario 'Current user highlighted when signed in' do
-		visit new_person_session_path
-		sign_in person
-		visit people_path
-		expect(page).to have_selector('.list-group-item-warning', text: person.name)
-		expect(page).to_not have_selector('.list-group-item-warning', text: person2.name)
-	end
+  scenario 'Current user highlighted when signed in' do
+    visit new_person_session_path
+    sign_in person
+    visit people_path
+    expect(page).to have_selector('.list-group-item-warning', text: person.name)
+    expect(page).to_not have_selector('.list-group-item-warning', text: person2.name)
+  end
 end
