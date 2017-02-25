bd818f2d48da66af2fb7cab51570a3c2d10cb3ac
berlintam <tam.eastley@gmail.com>
2017-02-25 15:58:44 +0100

    fixed specs in workshop_coach_spec

--- spec/features/workshop_coach_spec.rb
+++ spec/features/workshop_coach_spec.rb
@@ -30,13 +30,13 @@ feature 'workshop coach' do
 
   def visit_profile_page_of_a_workshop_coach
     person = create(:person, first_name: 'Sara', workshop_coach: true)
-    click_link 'All people'
+    click_link 'People'
     click_link 'Sara'
   end
 
   def visit_profile_of_normal_person
     person = create(:person, first_name: 'Foobar', workshop_coach: false)
-    click_link 'All people'
+    click_link 'People'
     click_link 'Foobar'
   end
 
