892559c384b438f0067d71fbeadc949748e73eca
Franziska Schmidt <hello@franka.tech>
2017-11-14 21:51:38 +0100

    Use mapbox for geocoder
    fix syntax
    
    Config for geocoder
    
    update
    
    Use Rails config for secret.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -5,5 +5,6 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.default_cassette_options = { :record => :new_episodes}
   c.allow_http_connections_when_no_cassette = false
+  c.filter_sensitive_data('<mapbox_api_key>') { Rails.application.secrets.mapbox_api_key }
   c.configure_rspec_metadata!
-end
+  end
