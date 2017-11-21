6398ad0b2691722658c5bd4ad5a5918914e48aa6
Franka <vsmart@users.noreply.github.com>
2017-11-21 20:36:48 +0100

    Don't allow http connections during specs (#579)

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,5 +4,6 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :webmock
   c.default_cassette_options = { :record => :new_episodes}
+  c.allow_http_connections_when_no_cassette = false
   c.configure_rspec_metadata!
 end
