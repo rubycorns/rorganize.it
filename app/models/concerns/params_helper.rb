89c8ba1ec3f636f42d13fb1f4fd1310f12beb0ee
Franziska Schmidt <franka.schmidt@mapbox.com>
2018-10-23 20:31:40 +0200

    format

--- app/models/concerns/params_helper.rb
+++ app/models/concerns/params_helper.rb
@@ -2,5 +2,6 @@ module ParamsHelper
   extend ActiveSupport::Concern
 
   def trim_params(params)
-    params.each { |_, val| val.strip! if val.respond_to?(:strip!) }   end
+    params.each { |_, val| val.strip! if val.respond_to?(:strip!) }
+  end
 end
