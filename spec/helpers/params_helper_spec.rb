bbd8b8765abd8f017fc3808518139e93a5a58d88
Franziska Schmidt <franka.schmidt@mapbox.com>
2018-11-20 21:07:37 +0100

    fix test

--- spec/helpers/params_helper_spec.rb
+++ spec/helpers/params_helper_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
+include ParamsHelper
 
 RSpec.describe ParamsHelper, type: :helper do
-  describe 'testing params' do
+  describe '#trim_params' do
+    input_params = { country: '  DE', name: ' franka ', email: ' hi@email.com', website: 'https://mywebsite.com '}
+    expected_params = { country: 'DE', name: 'franka', email: 'hi@email.com', website: 'https://mywebsite.com'}
+    params = helper.trim_params(input_params)
 
-    context 'trim_params' do
-      input_params = { country: 'DE', name: ' franka ', email: ' hi@email.com', website: 'https://mywebsite.com '}
-      expected_params = { country: 'DE', name: 'franka', email: 'hi@email.com', website: 'https://mywebsite.com'}
-#      params = helper.trim_params(input_params)
-      expect(helper.trim_params(input_params)).to equal(expected_params)
+    it "trims all params" do
+      expect(params).to eq(expected_params)
     end
   end
 end
