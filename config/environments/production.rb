f3520586b77c54e05f0485c237b9c1072b29c4cd
Franziska Schmidt <franka.schmidt@mapbox.com>
2019-03-05 21:22:06 +0100

    update config for @lislis review

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
   # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
   # `config/secrets.yml.key`.
-  config.read_encrypted_secrets = true
+  config.read_encrypted_secrets = false
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -65,6 +65,22 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = {
+    host: 'rorganize.it',
+    protocol: 'https'}
+
+  config.action_mailer.delivery_method = :sendmail
+
+  config.action_mailer.perform_deliveries = true
+
+  config.middleware.use ExceptionNotification::Rack,
+    email: {
+    email_prefix: "[RORGANIZE ERROR]",
+    sender_address: %{"Roar" <ror@schedar.uberspace.de>},
+    exception_recipients: %w[roar@rorganize.it]
+  },
+  error_grouping: true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
