825ce93377c3a5ef28eb0d3309b9421fcde219bc
lislis <mail@lislis.de>
2019-08-20 20:48:53 +0200

    rails 5.2.3

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 ruby '2.5.5'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.6'
+gem 'rails', '~> 5.2.3'
 
 gem 'devise'
 
--- config/application.rb
+++ config/application.rb
@@ -9,13 +9,14 @@ Bundler.require(*Rails.groups)
 module RailsGirlsApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
 
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|
       "<div class=\"field_with_errors has-error\">#{html_tag}</div>".html_safe
     end
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
